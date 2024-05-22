261e547e8a52a255777a0fcb4a074ce885c13eb3
Yusuf Roqib <114329528+yusufroqib@users.noreply.github.com>
2024-05-22 23:05:07 +0100

    first commit

---
+++ src/contract/index.js
@@ -0,0 +1,4 @@
+import contract from './AVAXGods.json';
+
+export const ADDRESS = '0x7855a298A40de26A2ba485213606a34F3084b980';
+export const { abi: ABI } = contract;
---
+++ src/utils/Onboard.js
@@ -0,0 +1,115 @@
+/* eslint-disable prefer-destructuring */
+function isEthereum() {
+  if (window.ethereum) {
+    return true;
+  }
+  return false;
+}
+
+function getChainID() {
+  if (isEthereum()) {
+    return parseInt(window.ethereum.chainId, 16);
+  }
+  return 0;
+}
+
+async function handleConnection(accounts) {
+  if (accounts.length === 0) {
+    const fetchedAccounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+    return fetchedAccounts;
+  }
+
+  return accounts;
+}
+
+async function requestAccount() {
+  let currentAccount = 0x0;
+  if (isEthereum() && getChainID() !== 0) {
+    let accounts = await window.ethereum.request({ method: 'eth_accounts' });
+    accounts = await handleConnection(accounts);
+    currentAccount = accounts[0];
+  }
+  return currentAccount;
+}
+
+async function requestBalance(currentAccount) {
+  let currentBalance = 0;
+  if (isEthereum()) {
+    try {
+      currentBalance = await window.ethereum.request({
+        method: 'eth_getBalance',
+        params: [currentAccount, 'latest'],
+      });
+
+      currentBalance = parseInt(currentBalance, 16) / 1e18;
+
+      return { currentBalance, err: false };
+    } catch (err) {
+      return { currentBalance, err: true };
+    }
+  }
+  return { currentBalance, err: true };
+}
+
+export const GetParams = async () => {
+  const response = {
+    isError: false,
+    message: '',
+    step: -1,
+    balance: 0,
+    account: '0x0',
+  };
+
+  if (!isEthereum()) {
+    response.step = 0;
+    return response;
+  }
+
+  const currentAccount = await requestAccount();
+  if (currentAccount === 0x0) {
+    response.step = 1;
+    return response;
+  }
+
+  response.account = currentAccount;
+
+  if (getChainID() !== 4202) {
+    response.step = 2;
+    return response;
+  }
+
+  const { currentBalance, err } = await requestBalance(currentAccount);
+  if (err) {
+    response.isError = true;
+    response.message = 'Error fetching balance!';
+    return response;
+  }
+  response.balance = currentBalance;
+  console.log(currentBalance)
+
+  if (currentBalance < 0.001) {
+    response.step = 3;
+    return response;
+  }
+
+  return response;
+};
+
+export async function SwitchNetwork() {
+  await window?.ethereum?.request({
+    method: 'wallet_addEthereumChain',
+    params: [ {
+			chainId: `0x${Number(4202).toString(16)}`,
+			chainName: "Lisk Sepolia Testnet",
+			nativeCurrency: {
+				name: "Sepolia Ether",
+				symbol: "ETH",
+				decimals: 18,
+			},
+			rpcUrls: ["https://rpc.sepolia-api.lisk.com"],
+			blockExplorerUrls: ["https://sepolia-blockscout.lisk.com"],
+		}],
+  }).catch((error) => {
+    console.log(error);
+  });
+}
